18a81c8a375687e2812e1c5560b06f12bd5b4025
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-11-17 00:02:58 -0500

    agregando ej5a

---
+++ cap3/ej5a.cpp
@@ -0,0 +1,13 @@
+/*******************************************************************************
+* Escriba una funcion que con entrada k imprima los k-numeros de Fibonacci *****
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	
+	
+	return 0;
+}
